6713ccd0fa265af5712a319dba9229e612fc4e38
Braian Maglio <braianmaglio76@gmail.com>
2024-07-04 20:58:04 -0300

    se realiza ejercicio03

--- ejercicio03.js
+++ ejercicio03.js
@@ -1,2 +1,7 @@
 // Ejercicio 3
 // Imprimir un listado con los números del 1 al 100 cada uno con su respectivo cuadrado
+
+
+for (let i = 1; i <= 100; i++) {
+  console.log(`${i} cuadrado: ${i * i}`);
+}
