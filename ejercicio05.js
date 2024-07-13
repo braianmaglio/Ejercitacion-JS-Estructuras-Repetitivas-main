5f0f1becabec463949fa2adf569180f80c617bac
Braian Maglio <braianmaglio76@gmail.com>
2024-07-12 22:11:45 -0300

    Se realiza el ejercicio05

--- ejercicio05.js
+++ ejercicio05.js
@@ -1,3 +1,9 @@
 // Ejercicio 5
 // Imprimir los números pares desde N (siendo N un número par que se lee) en forma descendente
 // hasta 2.
+
+let N = 20
+
+for (let i = N; i >= 2; i -= 2) {
+    console.log(i);
+}
