02483b11a63d0724a45ac6b6d8584c40ea5c3e26
Braian Maglio <braianmaglio76@gmail.com>
2024-07-13 20:25:21 -0300

    Se resuelve ejercicio06

--- ejercicio06.js
+++ ejercicio06.js
@@ -1,2 +1,21 @@
 // Ejercicio 6
 // Realizar la división entera a/b de dos valores enteros positivos mediante restas.
+let a
+let b
+
+function divisionEntera(a, b) {
+    if (b === 0) {
+        throw new Error("Divisor no puede ser cero");
+    }
+    
+    let resultado = 0;
+
+    while (a >= b) {
+        a -= b;
+        resultado++;
+    }
+
+    return resultado;
+}
+
+console.log(divisionEntera(15, 3));
