392e84abfb92029c3a09a721d20eefc7b2b6a9d0
Braian Maglio <braianmaglio76@gmail.com>
2024-07-29 18:58:25 -0300

    Se realiza ejercicio10

--- ejercicio10.js
+++ ejercicio10.js
@@ -1,3 +1,15 @@
 // Ejercicio 10
 // Imprimir los 100 primeros números de Fibonacci. Recuerde que un número de Fibonacci se calcula
 // como la suma de los dos anteriores así: 0, 1, 1, 2, 3, 5, 8, 13...
+
+let fibonacci = [0, 1];
+
+
+while (fibonacci.length < 100) {
+    const suma = fibonacci[fibonacci.length - 1] + fibonacci[fibonacci.length - 2];
+    fibonacci.push(suma);
+}
+
+let listado = fibonacci.toString();
+
+console.log(listado);
